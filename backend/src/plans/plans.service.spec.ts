8ff0fb99caa78de93e3c41a212f3e3c5e9c119a3
Bogdan <litvinenkob16@gmail.com>
2026-04-05 15:42:50 +0300

    fix imports in test

--- backend/src/plans/plans.service.spec.ts
+++ backend/src/plans/plans.service.spec.ts
@@ -1,7 +1,4 @@
-import {
-  ConflictException,
-  NotFoundException,
-} from '@nestjs/common';
+import { ConflictException, NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { Plan } from 'src/generated/prisma/client';
 import { PlanKey } from 'src/generated/prisma/enums';
